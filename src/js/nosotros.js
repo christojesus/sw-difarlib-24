45a2d8ccf266c9fc80da91b990cefec95b5db39f
Christopher Jesus <christopher3carrasco@outlook.com>
2024-01-17 10:13:37 -0500

    feat: add about us

---
+++ src/js/nosotros.js
@@ -0,0 +1,14 @@
+const historia = "Desde nuestro inicio en 1998, DIFARLIB se ha dedicado con fervor a la distribución de medicamentos a farmacias, boticas e instituciones de salud en Trujillo, La Libertad, Perú. Nuestra fundación se basó en el compromiso de ofrecer productos farmacéuticos en condiciones óptimas, respaldados por una calidad de servicio excepcional, y con la clara intención de ser reconocidos por nuestras \"Entregas Confiables\". A lo largo de los años, hemos consolidado nuestra presencia en el mercado, estableciendo estándares de confiabilidad que nos han convertido en un referente en la industria."
+const mision = "En DIFARLIB, nuestra misión es distribuir productos farmacéuticos de reconocida calidad a precios justos, garantizando entregas inmediatas a pequeñas, medianas y grandes farmacias, boticas, cadenas de farmacias e instituciones de la región. Contribuimos activamente a la protección de la salud de la comunidad, respaldados por un equipo de profesionales con alta calidad humana, reconocida trayectoria y amplio conocimiento del mercado. Nuestro enfoque colaborativo nos permite trabajar en equipo, asegurando la satisfacción de nuestros clientes y fortaleciendo nuestra posición en el sector."
+const vision = "Nos proyectamos como la droguería especializada líder en el Nor-Oriente del país, cumpliendo rigurosamente con las Buenas Prácticas de Almacenamiento (BPA) y estándares de distribución. En DIFARLIB, aspiramos a ser los mejores aliados de nuestros clientes, liderando el mercado de Distribución de Productos Farmacéuticos. Contamos con un personal altamente capacitado, motivado y plenamente identificado con nuestra empresa, lo que nos permite brindar un servicio de calidad que consolide nuestra posición como referente en la región. Estamos comprometidos con el crecimiento sostenido y la excelencia en cada aspecto de nuestras operaciones."
+const url = "https://www.youtube.com/watch?v=traqkcVFUqY"
+
+const elementHistoria = document.getElementById("historia")
+const elementMision = document.getElementById("mision")
+const elementVision = document.getElementById("vision")
+const elementUrl = document.getElementById("url")
+
+elementHistoria.innerText = historia
+elementMision.innerText = mision
+elementVision.innerText = vision
+elementUrl.innerText = url
