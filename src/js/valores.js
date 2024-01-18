dd05d9e69058709d62b07afaa48071fe4ef4b06c
Christopher Jesus <christopher3carrasco@outlook.com>
2024-01-18 08:45:13 -0500

    feat: add our values

---
+++ src/js/valores.js
@@ -0,0 +1,14 @@
+const vocacion = "Nos caracteriza una preocupación constante por ofrecer un servicio de calidad excepcional a nuestros clientes. Entendemos que la satisfacción del cliente es el motor de nuestro éxito y, por lo tanto, nos esforzamos continuamente por superar sus expectativas."
+const integridad = "En DIFARLIB, la integridad es la piedra angular de nuestra empresa. Operamos con honestidad y responsabilidad en todas nuestras interacciones, tanto internas como externas. Nuestro equipo está compuesto por individuos comprometidos con altos estándares éticos."
+const capacitacion = "Reconocemos la importancia del conocimiento y la innovación en nuestra industria. Por ello, invertimos en la constante capacitación de nuestro personal. Fomentamos la creatividad y el desarrollo personal, asegurándonos de que nuestro equipo esté siempre actualizado con las últimas tendencias y avances en el sector farmacéutico."
+const flexibilidad = "En un mercado dinámico, somos conscientes de la necesidad de adaptarnos a los cambios y ser flexibles ante los requerimientos del entorno. Esta flexibilidad nos permite anticiparnos a las demandas del mercado y ofrecer soluciones ágiles, garantizando así nuestro posicionamiento como líderes en la distribución de productos farmacéuticos en el Nor-Oriente del país."
+
+const elementVocacion = document.getElementById("vocacion")
+const elementIntegridad = document.getElementById("integridad")
+const elementCapacitacion = document.getElementById("capacitacion")
+const elementFlexibilidad = document.getElementById("flexibilidad")
+
+elementVocacion.innerText = vocacion
+elementIntegridad.innerText = integridad
+elementCapacitacion.innerText = capacitacion
+elementFlexibilidad.innerText = flexibilidad
