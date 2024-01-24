ebdc8f97dbc088bf5d10cd649a9e0e667b3bcccd
Christopher Jesus <christopher3carrasco@outlook.com>
2024-01-24 12:31:38 -0500

    feat: add carousel

---
+++ src/js/carousel.js
@@ -0,0 +1,11 @@
+const photo1 = 'img/photo-difarlib-1.png'
+const photo2 = 'img/photo-difarlib-2.png'
+const photo3 = 'img/photo-difarlib-3.png'
+
+const elementImage1 = document.getElementById("carousel-image-1")
+const elementImage2 = document.getElementById("carousel-image-2")
+const elementImage3 = document.getElementById("carousel-image-3")
+
+elementImage1.src = photo1
+elementImage2.src = photo2
+elementImage3.src = photo3
